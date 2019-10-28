f5f30f360062b5c5a81f388dd3bac6889a174f12
justthedoctor <justthedoctor.931@gmail.com>
2019-10-28 15:04:52 -0500

    v0.0.2a

--- index.js
+++ index.js
@@ -10,8 +10,8 @@
   var urlencodedParser = bodyParser.urlencoded({ extended: true });
   var express = require('express');
   var app = express();
-  cvaVersion = "0.0.2";
-
+  cvaVersion = "0.0.2a";
+  defaultlookupAddress = "PEmgkyPnU7GCr4CCRU9hi4v2AmwBiQy6JM";
 
           // Set the Pug Engine and Pug Source DIR
     app.set("view engine", "pug");
@@ -22,29 +22,44 @@
         var httpHost = "localhost";
 
       var server = app.listen(httpPort, httpHost, function () {
-          console.log('HTTP running on http://' + httpHost + ':' + httpPort);
-          console.log('Version:'+ cvaVersion);
+        console.log('Crypto-Viewer Version:'+ cvaVersion);
+        console.log('HTTP running on http://' + httpHost + ':' + httpPort);
       });
 
           // Gather information for the setup of which coin network to read from
       request.get("https://chainz.cryptoid.info/explorer/api.dws?q=summary", (error, response, body) => {
+        if(error) {
+          summaryError = error;
+        }
+
         summaryParse = JSON.parse(body);
         summaryStringify = JSON.stringify(body);
         summaryCoinName = Object.values(summaryParse).map(entry => entry.name);
+        summaryTicker = Object.keys(summaryParse);
+        summaryCoinTickerName = Object.entries(summaryParse).map(([key, obj]) => ({key, name: obj.name}))
       })
+
+      request.get(`https://chainz.cryptoid.info/pnd/api.dws?q=multiaddr&key=1a9c92c7492b&active=${defaultlookupAddress}`, (error, response, body) => {
+        if(error) {
+          addressError = error;
+        }
+
+        addressParse = JSON.parse(body);
+        addressStringify = JSON.stringify(body);
+      });
 
           // Setup the function of replying to http://httpHost:httpPort/
       app.use('/public', express.static('public'))
 
       app.get("/", (req, res) => {
-          res.render("index");
+        res.render("index");
       });
 
-      app.post("/opreturn", urlencodedParser, (req, res) => {
+      app.get("/opreturn", (req, res) => {
         res.render("opreturn");
       });
 
           // Setup the function of replying to http://httpHost:httpPort/
       app.get("/admin", (req, res) => {
-          res.render("admin/index");
+        res.render("admin/index");
       });
